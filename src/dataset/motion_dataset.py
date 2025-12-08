8782e3f11749211b90bc5c515a143279a3585405
Anthony Roumi <tonyroumi@gmail.com>
2025-12-07 23:52:28 -0500

    Adds cross domain motion dataset

--- src/dataset/motion_dataset.py
+++ src/dataset/motion_dataset.py
@@ -1,145 +1,40 @@
+from .builder import MotionDatasetBuilder
 from .normalization import NormalizationStats
 
-from src.data_processing import DataSourceAdapter
-from src.utils.data import _to_torch
-from src.data_processing import SkeletonMetadata, MotionSequence
-
 from torch.utils.data import Dataset
-from typing import List, Any, Dict
-import numpy as np
+from typing import List
 import torch
-
-
-class MotionDatasetBuilder:
-    """ MotionDataset builder to construct model Dataset object. """
-    def __init__(
-        self, 
-        adapter: DataSourceAdapter, 
-        data_config: Dict[str, Any],
-        device: torch.device = 'cpu',
-    ):
-        self.adapter = adapter
-        self.data_config = data_config
-
-        self.window_size = data_config['window_size']
-        self.downsample_fps = data_config['downsample_fps']
-
-        self.device = device
-
-    def get_or_process(self, character: str):
-        """ Load or generate all motion data and skeleton metadata for a given character. """ 
-        cache_path = (
-            self.adapter.cache_dir / character
-        )
-        processed_files = list(cache_path.glob("*"))
-
-        raw_path = (
-            self.adapter.raw_dir / character
-        )
-        raw_files = list(raw_path.glob("*"))
-
-        if not raw_files:
-            raise FileNotFoundError(f"No raw files for {character}")
-
-        skel_path = (
-            self.adapter.skeleton_dir / f"{character}.npz"
-        )
-        # If skeleton doesn't exist, extract it. 
-        if not skel_path.exists():
-            skeleton = self.adapter.extract_skeleton(character)
-        else:
-            skeleton = SkeletonMetadata.load(skel_path)
-        
-        offsets = skeleton.offsets
-        topology = skeleton.topology
-        ee_ids = skeleton.ee_ids
-        height = skeleton.height
-
-        # If there are no processed files, proccess from raw dir. 
-        if not processed_files:
-            motions = self.adapter.extract_motion(character)
-        else:
-            all_motion = []
-            for proccesed_motion in processed_files:
-                motion = MotionSequence.load(proccesed_motion)
-
-                rotations = motion.rotations
-                rotations = rotations.reshape(rotations.shape[0], -1)
-                root_orient = motion.root_orient
-
-                full_motion = np.hstack([root_orient, rotations])
-                full_motion = self._downsample(motion=full_motion, 
-                                               fps_in=motion.fps)
-
-                windowed_motion = self._get_windows(full_motion)
-
-                all_motion.append(windowed_motion)
-
-        all_motion = np.vstack(all_motion)
-
-        return (_to_torch(all_motion, self.device),
-                _to_torch(offsets, self.device),
-                _to_torch(topology, self.device),
-                _to_torch(ee_ids, self.device),
-                _to_torch(height, self.device))
-
-    def _downsample(self, motion: np.ndarray, fps_in: float):
-        """
-        Downsample by uniform subsampling.
-        fps_out must be <= fps_in.
-        """
-        if self.downsample_fps > fps_in:
-            raise ValueError("fps_out must be <= fps_in for simple subsampling.")
-
-        stride = int(round(fps_in / self.downsample_fps))
-        return motion[::stride]
-
-    def _get_windows(self, motion: np.ndarray):
-        """ 
-        Slice a motion array of shape [T, J*4+7] into windows of shape [window_size, J*4+7].
-
-        Returns:
-          [num_windows, window_size, J*4+7].
-        """
-        T = motion.shape[0]
-        ws = self.window_size
-
-        num_windows = T // ws
-        out = []
-
-        for i in range(num_windows):
-            start = i * ws
-            end = start + ws
-            out.append(motion[start:end])
-
-        return np.stack(out)
 
 class MotionDataset(Dataset):
     """
-    Represents all motion windows for a motion domain where all characters share the same topology
+    Represents all motion for a motion domain where all characters share the same topology
     and end-effectors.
 
-    Each sample is:
-       rotations: [window, n_joints*4]
-       offsets: ?
+    Each sample contains:
+
+        motion: Windowed motion from all characters in the dataset.
+        offsets: Offsets of only the characters in the motion dataset stacked for batch size.
+        height: Heights of only the characters in the motion dataset stacked for batch size. 
     """
 
     def __init__(
         self,
-        builder: MotionDatasetBuilder,
+        name: str,
         characters: List[str],
+        builder: MotionDatasetBuilder,
     ):
-        self.motion_seqs = []
-        self.offsets = []
+        self.name = name
+
         self.char_index = []         # maps each sample → char_id
-        self.char_meta = {}          # char_name → {offsets, height}
+        self.char_meta = {}
 
         shared_topology = None
         shared_ee_ids = None
 
+        motion_seqs = []
         for char_id, char in enumerate(characters):
             (
-                motion_windows,      # [num_windows, (T) window, 7+(J-1)*4]
+                motion_windows,
                 offsets,
                 topology,
                 ee_ids,
@@ -150,52 +45,74 @@ class MotionDataset(Dataset):
                 shared_topology = topology
                 shared_ee_ids = ee_ids
             else:
-                if topology != shared_topology:
+                if not torch.equal(topology, shared_topology):
                     raise ValueError(f"Topology mismatch for character {char}")
-                if ee_ids != shared_ee_ids:
+                if not torch.equal(ee_ids, shared_ee_ids):
                     raise ValueError(f"EE ids mismatch for character {char}")
 
             self.char_meta[char] = {
+                "offsets": offsets,
                 "height": height,
                 "id": char_id,
             }
-            self.motion_seqs.append(motion_windows)
-            self.offsets.append(offsets)
+            motion_seqs.append(motion_windows)
             self.char_index.extend([char_id] * motion_windows.shape[0])
 
-        self.motion_seqs = torch.cat(self.motion_seqs, dim=0)
-        self.offsets = torch.cat(self.offsets, dim=0)
+        self.samples = torch.cat(motion_seqs, dim=0)
         self.char_index = torch.tensor(self.char_index, dtype=torch.long)
 
         self.topology = shared_topology
         self.ee_ids = shared_ee_ids
 
         self.norm_stats = self._compute_normalization_stats()
+        self.samples = self.norm_stats.norm(self.samples)
 
+    def denorm(self, motion: torch.Tensor):
+        return self.norm_stats.denorm(motion)
+    
     def _compute_normalization_stats(self):
         # mean/var over batch and time
-        mean = torch.mean(self.samples, dim=(0, 2), keepdim=True)
-        var  = torch.var(self.samples, dim=(0, 2), keepdim=True)
+        mean = torch.mean(self.samples, dim=(0, 1), keepdim=True)
+        var  = torch.var(self.samples, dim=(0, 1), keepdim=True)
         return NormalizationStats(mean, var)
 
     def __len__(self):
         return self.samples.shape[0]
 
     def __getitem__(self, idx):
-        """
-        Returns:
-          rotations: Tensor [window, features]
-          metadata:  dict with per-character info (height, offsets, char_id)
-        """
         char_id = self.char_index[idx]
 
         # find which character this corresponds to
         char_name = list(self.char_meta.keys())[char_id]
         meta = self.char_meta[char_name]
 
-        return {
-            "motion": self.samples[idx],
-            "char_id": char_id,
-            "offsets": meta["offsets"],
-            "height": meta["height"],
+        return self.samples[idx], meta["offsets"], meta["height"]
+
+class CrossDomainMotionDataset(Dataset):
+    """ Combines two MotionDataset instances for two different motion domains. """
+    def __init__(
+        self,
+        domain_A: MotionDataset,
+        domain_B: MotionDataset
+    ):
+        self.domain_A = domain_A
+        self.domain_B = domain_B
+        self.domains = {
+            domain_A.name: domain_A,
+            domain_B.name: domain_B
         }
+
+    def denorm(self, domain_name: str, motion: torch.Tensor) -> torch.Tensor:
+        return self.domains[domain_name].denorm(motion)
+    
+    def __len__(self):
+        return max(len(self.domain_A, len(self.domain_B)))
+    
+    def __getitem__(self, idx):
+        idx_A = idx % len(self.domain_A)
+        idx_B = idx % len(self.domain_B)
+        
+        sample_A = self.domain_A[idx_A]
+        sample_B = self.domain_B[idx_B]
+
+        return sample_A, sample_B
--- src/dataset/normalization.py
+++ src/dataset/normalization.py
@@ -6,14 +6,6 @@ class NormalizationStats:
     mean: torch.Tensor
     var: torch.Tensor
 
-    def save(self, path):
-        torch.save({"mean": self.mean, "var": self.var}, path)
-
-    @staticmethod
-    def load(path):
-        d = torch.load(path)
-        return NormalizationStats(d["mean"], d["std"])
-
     def norm(self, x: torch.Tensor):
         idx = self.var < 1e-5 # Don't clip tiny variances
         self.var[idx] = 1
