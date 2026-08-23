419cf583276104a88ef94ad179bd12087267e027
Anthony Roumi <tonyroumi@gmail.com>
2026-08-22 22:32:52 -0700

    correct_gae

--- src/moveitmoveit/agents/ppo/ppo.py
+++ src/moveitmoveit/agents/ppo/ppo.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
-import math
+from abc import abstractmethod
 from typing import Any, Dict
+import math
+import os
 
 import torch
 import torch.nn as nn
@@ -18,27 +20,42 @@ from .ppo_cfg import PPOCfg
 from ..base import BaseAgent
 
 def compute_gae(
-    num_transitions_per_env: int,
     rewards: torch.Tensor,
     values: torch.Tensor,
+    last_values: torch.Tensor,
     dones: torch.Tensor,
     gae_lambda: float,
-    discount: float
+    discount: float,
+    normalize_advantage_per_mini_batch: bool
 )-> torch.Tensor:
-    advantages = torch.zeros_like(rewards)
-    for i in reversed(range(num_transitions_per_env-2, -1)):
-        td_error = (
-            rewards[i] + discount * (1 - dones[i]) * values[i+1] - values[i]
-        )
-        advantages[i] = td_error + gae_lambda * discount * advantages[i+1]
 
-    return advantages
+    advantage = 0
+    advantages = torch.zeros_like(rewards)
+    steps = rewards.shape[0]
+
+    for i in reversed(range(steps)):
+        next_values = values[i+1] if i < steps - 1 else last_values
+
+        advantage = (
+            rewards[i] - values[i] + discount * (1 - dones[i]) * (next_values + gae_lambda * advantage)
+        )
+
+        advantages[i] = advantage
+
+    returns = advantages + values
+
+    if (not normalize_advantage_per_mini_batch):
+        advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-8)
+
+    return advantages, returns
 
 class PPO(BaseAgent):
     cfg: PPOCfg
 
     def __init__(self, cfg: dict, logger: Logger):
         super().__init__(cfg=cfg, logger=logger)
+
+        self.current_timestep = 0
 
     def initialize_models(self, env: DirectRLEnv, model_cfg: dict) -> None:
         obs_size = env.observation_space.shape[-1]
@@ -54,7 +71,9 @@ class PPO(BaseAgent):
             **model_cfg["critic"]
         ).to(env.unwrapped.device)
 
-        self.obs_preproccessor = RunningStandardScaler(size=obs_size).to(env.unwrapped.device)
+        self.obs_preprocessor = RunningStandardScaler(size=obs_size).to(env.unwrapped.device)
+
+        self._initialize_optimizer()
 
     def initialize_storage(
         self,
@@ -71,107 +90,145 @@ class PPO(BaseAgent):
         )
         self.transition = self.storage.Transition()
 
-    def act(self, observations: torch.Tensor, deterministic: bool = False) -> torch.Tensor:
-        result = {
-            "observations": self.obs_preproccessor(observations, train=(not deterministic)),
-            "value": self.critic(observations)
-        }
-        actions = self.actor(result["observations"], deterministic=deterministic)
+        self._next_observations = None
 
-        self.transition.observations = observations
+    def _initialize_optimizer(self) -> None:
+        self.optimizer = torch.optim.Adam(
+            list(self.actor.parameters()) + list(self.critic.parameters()),
+            lr=self.cfg.learning_rate,
+        )
+
+    def act(self, observations: torch.Tensor, deterministic: bool = False) -> torch.Tensor:
+        normed_obs = self.obs_preprocessor(observations, train=(not deterministic))
+        
+        actions = self.actor(normed_obs, deterministic=deterministic)
+        values = self.critic(normed_obs)
+
+        self.transition.observations = normed_obs
         self.transition.actions = actions # sampled action
         self.transition.actions_log_prob = self.actor.get_actions_log_prob(actions) #log_prob
-        self.transition.values = result["value"]
+        self.transition.values = values
 
         return actions
     
     def process_env_step(
         self,
+        next_observations: torch.Tensor,
         rewards: torch.Tensor,
         terminated: torch.Tensor,
         timeout: torch.Tensor,
         infos: dict | None = None,
     ) -> None:
+        super().process_env_step(next_observations, rewards, terminated, timeout, infos)
+
         self.transition.rewards = rewards
         self.transition.dones = terminated
 
         self.storage.add_transition(self.transition)
+        self.transition.clear()
 
-    def update(self, optimizer: torch.optim.Optimizer) -> None:
-        metrics = {}
+        self._next_observations = next_observations
+
+    def update(self) -> None:
+        train_evals = {}
+
+        observations = self.obs_preprocessor(self._next_observations)
+        last_values = self.critic(self._next_observations)
 
         advantages = compute_gae(
-            self.storage.num_transitions_per_env,
             self.storage.rewards,
             self.storage.values,
+            last_values,
             self.storage.dones,
             self.cfg.gae_lambda,
-            self.cfg.discount
+            self.cfg.discount,
+            self.cfg.normalize_advantage_per_mini_batch
         )
-
-        if (not self.cfg.normalize_advantage_per_mini_batch):
-            advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-8)
 
         self.storage.advantages = advantages
 
-        for batch in self.storage.mini_batch_generator(
+        generator = self.storage.mini_batch_generator(
             self.cfg.num_mini_batches,
             self.cfg.num_learning_epochs
-        ):
+        )
+
+        for batch in generator:
             if (self.cfg.normalize_advantage_per_mini_batch):
                 advantage = (batch.advantages - batch.advantages.mean()) / (batch.advantages.std() + 1e-8)
+            else:
+                advantage = batch.advantages
 
-            observations = self.obs_preproccessor(batch.observations)
+            observations = batch.observations #self.obs_preprocessor(batch.observations)
             
             # update policy distribution
             self.actor(observations)
 
             # actor loss
-            actions_log_prob = self.actor.get_actions_log_prob(batch.actions)
-            surrogate = advantage * torch.exp(actions_log_prob - batch.old_actions_log_prob)
-            surrogate_clipped = surrogate.clip(1 - self.cfg.clip_param, 1 + self.cfg.clip_param)
+            actions_log_prob = self.actor.get_actions_log_prob(batch.actions).unsqueeze(-1)
+            surrogate = torch.exp(actions_log_prob - batch.old_actions_log_prob)
+            surrogate_clipped = advantage * torch.clamp(surrogate, 1-self.cfg.clip_param, 1+self.cfg.clip_param)
 
             # actor loss
-            actor_loss = -surrogate_clipped
-            # critic loss
-            value_loss = torch.mean(torch.pow(batch.returns - self.critic(observations)))
+            actor_loss = -surrogate_clipped.mean()
+            # critic loss. can also use td here
+            value_loss = torch.mean((batch.rewards - self.critic(observations))**2)
 
             if self.cfg.use_clipped_value_loss:
-                value_loss = value_loss.clip(1 - self.cfg.value_loss_clip_param, 1 + self.cfg.value_loss_clip_param)
+                value_loss = torch.clamp(value_loss, 1-self.cfg.value_loss_clip_param, 1+self.cfg.value_loss_clip_param)
 
-            loss = actor_loss + value_loss * self.cfg.value_loss_coef - self.cfg.entropy_coef * self.actor.entropy
+            loss = actor_loss + value_loss * self.cfg.value_loss_coef #- self.cfg.entropy_coef * self.actor.entropy
 
-            optimizer.zero_grad()
+            self.optimizer.zero_grad()
             loss.backward()
 
-            torch.nn.utils.clip_grad_norm_(
+            grad_norm = torch.nn.utils.clip_grad_norm_(
                 list(self.actor.parameters()) + list(self.critic.parameters()),
                 max_norm=self.cfg.max_grad_norm,
             )
 
-            old_dist = self.actor.dist
+            # old_dist = self.actor.dist
 
-            optimizer.step()
+            self.optimizer.step()
 
             self.grad_step += 1
 
-            new_dist = self.actor.dist
+            # new_dist = self.actor.dist
 
-            kl = torch.distributions.kl_divergence(
-                old_dist, new_dist
-            ).sum(dim=-1).mean()
+            # kl = torch.distributions.kl_divergence(
+            #     old_dist, new_dist
+            # ).sum(dim=-1).mean()
 
-            metrics.update({
-                "train/actor_loss": actor_loss,
-                "train/value_loss": value_loss,
-                "train/kl_divergence": kl,
-                "train/entropy": self.actor.entropy,
-                "train/advantage_mean": advantages.mean(),
-                "train/advantage_std": advantages.std(),
-                "train/value_estimate_mean": batch.values.mean(),
-                "train/value_estimate_std": batch.values.std(),
+            train_evals.update({
+                "Train/actor_loss": actor_loss.item(),
+                "Train/value_loss": value_loss.item(),
+                # "Train/kl_divergence": kl.item(),
+                # "Train/entropy": self.actor.entropy,
+                "Train/advantage_mean": advantage.mean().item(),
+                "Train/advantage_std": advantage.std().item(),
+                "Train/return_estimate_mean": batch.values.mean().item(),
+                "Train/grad_norm": grad_norm.item(),
             })
 
-            self.logger.log_scalars(metrics, self.grad_step)
+            self.logger.log_scalars(train_evals, self.grad_step)
 
         self.storage.clear()
+
+    def write_checkpoint(self, total_timesteps: int) -> None:
+        """Save the agent's models to the specified path."""
+        path = os.path.join(self.logger.log_dir, "checkpoints")
+        os.makedirs(path, exist_ok=True)
+
+        torch.save({
+            "actor": self.actor.state_dict(),
+            "critic": self.critic.state_dict(),
+            "obs_preprocessor": self.obs_preprocessor.state_dict(),
+            "optimizer": self.optimizer.state_dict(),
+        }, f"{path}/{self.env_step}_{total_timesteps}.pt")
+
+    def load_checkpoint(self, path: str, device: torch.device) -> None:
+        """Load the agent's models from the specified path."""
+        checkpoint = torch.load(path, map_location=device)
+        self.actor.load_state_dict(checkpoint["actor"])
+        self.critic.load_state_dict(checkpoint["critic"])
+        self.obs_preprocessor.load_state_dict(checkpoint["obs_preprocessor"])
+        self.optimizer.load_state_dict(checkpoint["optimizer"])
